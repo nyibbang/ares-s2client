6b352c77041a881730203369891c8fd064054a1d
Vincent PALANCHER <vincent.palancher@gmail.com>
2017-12-19 10:19:26 +0100

    Move build_structure into decision

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,6 +1,5 @@
 #include "bot.hpp"
 #include <sc2api/sc2_interfaces.h>
-#include <boost/optional.hpp>
 #include <iomanip>
 #include <iostream>
 #include "decision.hpp"
@@ -14,53 +13,12 @@ namespace ares {
 namespace {
 
 // TODO: automatically get builder_unit_type
-bool build_structure(Agent& agent, UNIT_TYPEID unit_type,
-                     UNIT_TYPEID builder_unit_type = UNIT_TYPEID::TERRAN_SCV,
-                     boost::optional<Point2D> position = {}) {
-  const auto* obs = agent.Observation();
-
-  const auto unit_typedata = obs->GetUnitTypeData()[UnitTypeID(unit_type)];
-  const auto ability_id =
-      GetGeneralizedAbilityID(unit_typedata.ability_id, *obs);
-
-  if (obs->GetMinerals() < unit_typedata.mineral_cost ||
-      obs->GetVespene() < unit_typedata.vespene_cost) {
-    return false;
-  }
-
-  // If a unit already is building a supply structure of this type, do nothing.
-  // Also get an scv to build the structure.
-  const auto units =
-      obs->GetUnits(Unit::Alliance::Self, IsUnit(builder_unit_type));
-  for (const auto* unit : units) {
-    for (const auto& order : unit->orders) {
-      if (order.ability_id == ability_id) {
-        return false;
-      }
-    }
-  }
-
-  if (units.empty()) {
-    return false;
-  }
-
-  const auto* builder = units.front();
-
-  if (!position) {
-    position = find_new_building_position(builder->pos);
-  }
-
-  std::cout << "Building structure " << UnitTypeToName(unit_type) << std::endl;
-  agent.Actions()->UnitCommand(builder, ability_id, *position);
-  return true;
-}
-
 void decide_to_build_supply_depot(Agent& agent) {
   const auto* obs = agent.Observation();
 
   // If we are not supply capped, don't build a supply depot.
   if (obs->GetFoodUsed() > obs->GetFoodCap() - 2) {
-    build_structure(agent, UNIT_TYPEID::TERRAN_SUPPLYDEPOT);
+    build_structure(*obs, *agent.Actions(), UNIT_TYPEID::TERRAN_SUPPLYDEPOT);
   }
 }
 
--- src/decision.cpp
+++ src/decision.cpp
@@ -1,9 +1,60 @@
 #include "decision.hpp"
 #include <sc2api/sc2_common.h>
+#include <iostream>
 
 using namespace sc2;
 
 namespace ares {
+
+bool build_structure(const ObservationInterface& obs,
+                     ActionInterface& act, UNIT_TYPEID unit_type,
+                     UNIT_TYPEID builder_unit_type,
+                     boost::optional<Point2D> position) {
+  const auto unit_typedata = obs.GetUnitTypeData()[UnitTypeID(unit_type)];
+  const auto ability_id =
+      GetGeneralizedAbilityID(unit_typedata.ability_id, obs);
+
+  if (obs.GetMinerals() < unit_typedata.mineral_cost ||
+      obs.GetVespene() < unit_typedata.vespene_cost) {
+    return false;
+  }
+
+  // If a unit already is building a supply structure of this type, do nothing.
+  // Also get an scv to build the structure.
+  // TODO: choosing a unit to build the structure should be part of decision
+  const auto units =
+      obs.GetUnits(Unit::Alliance::Self, IsUnit(builder_unit_type));
+  for (const auto* unit : units) {
+    for (const auto& order : unit->orders) {
+      if (order.ability_id == ability_id) {
+        return false;
+      }
+    }
+  }
+
+  if (units.empty()) {
+    return false;
+  }
+
+  const auto* builder = units.front();
+
+  if (!position) {
+    position = find_new_building_position(builder->pos);
+  }
+
+  std::cout << "Building structure " << UnitTypeToName(unit_type) << std::endl;
+  act.UnitCommand(builder, ability_id, *position);
+  return true;
+}
+
+bool Build_supply_depot::condition(const ObservationInterface& obs) const {
+  return obs.GetFoodUsed() > obs.GetFoodCap() - 2;
+}
+
+void Build_supply_depot::execute(const ObservationInterface& obs,
+                                 ActionInterface& act) {
+  build_structure(obs, act, UNIT_TYPEID::TERRAN_SUPPLYDEPOT);
+}
 
 Point2D find_new_building_position(const Point2D& center) {
   // TODO: decide of a position instead of picking a random one
